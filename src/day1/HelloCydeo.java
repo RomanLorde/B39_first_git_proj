834a9ff72e70b5489bea42cc90ce3aba1b7ad507
RomanLorde <88249345+RomanLorde@users.noreply.github.com>
2025-03-02 11:16:06 -0500

    add ms in cydeo class

--- src/day1/HelloCydeo.java
+++ src/day1/HelloCydeo.java
@@ -3,5 +3,7 @@ package day1;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+                System.out.println("Create this line in remote rep");
+
     }
 }
